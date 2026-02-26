0323d7d1baac8ae4343842296ecfbd9032424a83
Esteban <estebansobaki@gmail.com>
2026-02-26 17:04:52 -0600

    Migración a PHP, Inter, CSS, Footer WIP
    Migración de HTML a PHP
    Bootstrap integrado
    Fuente Inter de Google integrada
    CSS base configurado
    Imágenes redes sociales (footer)

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BuscoBrete</title>
+        <link rel="stylesheet" href="./css/styles.css">
+
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">    
+        <!-- Bootstrap JS Bundle -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+        <!-- Google Fonts-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <!-- Inter font -->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <header>
+            <h1>Busco Brete</h1>
+        </header>
+
+        <main>
+            <section>
+
+            </section>
+            <section>
+            
+            </section>
+        </main>
+
+        <footer> <!-- Poner esto relativo a la pantalla, en la parte inferior -->
+            <img src="./img/social_icons.png" alt="Redes Sociales"> 
+        </footer>
+    </body>
+
+</html>
